a745946b3b30e71834a8378d3364e9fcc7616ce7
niklas <oz3angam3r@gmail.com>
2023-05-03 14:36:24 +0200

    Versuche zählen

--- ZahlenRaten/ZahlenRaten.py
+++ ZahlenRaten/ZahlenRaten.py
@@ -4,15 +4,20 @@ import random
 class Zahlenraten():
 
     def __init__(self) -> None:
-        self.computer_zahl = random.randint(1000, 9999)
-        self.bereits_gewonnen = False
+        self.computer_zahl: int = random.randint(1000, 9999)
+        self.bereits_gewonnen: bool = False
+        self.versuche: int = 0
 
     def raten(self, geratene_zahl: int) -> None:
-
+        self.versuche += 1
         self.bereits_gewonnen = (geratene_zahl == self.computer_zahl)
 
     def wurde_bereits_gewonnen(self) -> bool:
         return self.bereits_gewonnen
 
     def anzahl_versuche(self) -> int:
+
+        return self.versuche
+
+    def passende_stellen(self) -> list[int]:
         pass
--- ZahlenRaten/test_ZahlenRaten.py
+++ ZahlenRaten/test_ZahlenRaten.py
@@ -16,6 +16,20 @@ class TestZahlenRaten(unittest.TestCase):
             zr.raten(1234)
             self.assertTrue(zr.wurde_bereits_gewonnen())
 
+    def test_anzahl_der_versuche(self):
+
+        zr = Zahlenraten()
+        zr.anzahl_versuche()
+        zr.raten(1234)
+        zr.raten(329429)
+        self.assertEqual(zr.anzahl_versuche(), 2)
+
+    def test_falsch_geraten(self):
+        with mock.patch("random.randint", return_test_number):
+            zr = Zahlenraten()
+            zr.raten(4321)
+            self.assertFalse(zr.wurde_bereits_gewonnen())
+
 
 if __name__ == "__main__":
     unittest.main()
