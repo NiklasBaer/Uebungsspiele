131b1bbbe91a4b11e6d6073899680091973a8a04
niklas <oz3angam3r@gmail.com>
2023-05-04 08:54:15 +0200

    Kommentare eingefügt

--- ZahlenRaten/ZahlenRaten.py
+++ ZahlenRaten/ZahlenRaten.py
@@ -3,10 +3,15 @@ import random
 
 class Zahlenraten():
 
+    # Definiert alle benötigten Variablen
+
     def __init__(self) -> None:
         self.computer_zahl: int = random.randint(1000, 9999)
         self.bereits_gewonnen: bool = False
         self.versuche: int = 0
+
+    # zählt die versuche, schaut ob bereits gewonnen
+    # legt die lsten für die beidenen jewaligen zahlen an
 
     def raten(self, geratene_zahl: int) -> None:
         self.versuche += 1
@@ -16,13 +21,24 @@ class Zahlenraten():
         self.computer_zahl_liste = self.zahl_als_ziffern_liste(
             self.computer_zahl)
 
+    # gibt ein True oder False zurück
+
     def wurde_bereits_gewonnen(self) -> bool:
 
         return self.bereits_gewonnen
 
+    # gibt die Anzahl der Versche zurück
+
     def anzahl_versuche(self) -> int:
 
         return self.versuche
+
+    # überprüft ob ob zewi zahlen mit dem gleichen Index das gleiche element haben
+    # falls dies der fall ist füngen wir den index (an welcher stelle das ist) in eine liste in
+    # Beispiel wir haben die Listen [1,2,3,4] und [1,3,2,4]
+    # dann sieht mann das die erste und letzte zahl gleich sind
+    # aus der funktion würden wir so eine liste bekommen [0, 3]
+    # (beachte in PYthon wird mit 0 angefangen 0 ist die erste Stelle)
 
     def passende_stellen(self) -> list[int]:
         ergebnis_liste: list[int] = []
@@ -35,6 +51,8 @@ class Zahlenraten():
                 ergebnis_liste.append(idx)
 
         return ergebnis_liste
+
+    #   gibt die liste zurück
 
     def zahl_als_ziffern_liste(self, zahl: int) -> list[int]:
 
